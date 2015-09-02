76831fa317c2c8a1e611983e1e2fa91afc567757
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2015-09-01 21:27:43 -0300

    libMatchmakerData: CustomObject

--- libMatchmakerData/include/matchmakerdata/customobject.h
+++ libMatchmakerData/include/matchmakerdata/customobject.h
@@ -5,11 +5,13 @@
 #include <boost/cast.hpp>
 #include <string>
 #include <map>
+#include <stdexcept>
+
 namespace mm
 {
 class Unknown_Field {}; //exception
+class Null_Field{};
 class CustomObject;
-typedef boost::optional<boost::any> CustomValue;
 };
 
 using namespace mm;
@@ -19,11 +21,46 @@ class mm::CustomObject
 public:
     CustomObject() {}
     ~CustomObject() {}
-    CustomValue get(std::string fieldName);
+
+    //throws mm::Unknown_Field and boost::bad_any_cast
+    //returns false opetional if value is null
+    template<typename T> boost::optional<T> get(std::string fieldName);
     void addField(std::string fieldName);
-    void set(std::string fieldName, boost::any fieldValue);
+    template<typename T>
+    void set(std::string fieldName, T fieldValue);
 
 private:
-    std::map<std::string, CustomValue> customFields_;
+    std::map<std::string, boost::optional<boost::any> > customFields_;
 };
+
+template <typename T> boost::optional<T> mm::CustomObject::get(std::string fieldName)
+{
+    boost::optional<boost::any> val;
+
+    try{
+        val = customFields_.at(fieldName);
+    }
+    catch(std::out_of_range error){
+        throw mm::Unknown_Field();
+    }
+
+    if(!val){
+        return boost::none;
+    }
+
+    T ret = boost::any_cast<T>(*val);
+    return ret;
+}
+
+template <typename T> void mm::CustomObject::set(std::string fieldName, T value)
+{
+    try{
+        boost::optional<boost::any> val = customFields_.at(fieldName);
+    }
+    catch(std::out_of_range error){
+        throw mm::Unknown_Field();
+    }
+
+    customFields_[fieldName] = boost::any(value);
+}
 #endif //MATCHMAKERDATA_H
--- libMatchmakerData/src/customobject.cpp
+++ libMatchmakerData/src/customobject.cpp
@@ -1,36 +1,11 @@
 #include "../include/matchmakerdata/customobject.h"
-#include <stdexcept>
+
 using namespace mm;
 void mm::CustomObject::addField(std::string fieldName)
 {
-    //CustomValue customValue = boost::none;
-
     customFields_[fieldName] = boost::none;
-    //customFields_.insert(std::pair<std::string, CustomValue>(fieldName, boost::none));
 }
 
-mm::CustomValue mm::CustomObject::get(std::string fieldName)
-{
-    CustomValue val;
 
-    try{
-        val = customFields_.at(fieldName);
-    }
-    catch(std::out_of_range error){
-        throw mm::Unknown_Field();
-    }
 
-    return val;
-}
 
-void mm::CustomObject::set(std::string fieldName, boost::any value)
-{
-    try{
-        CustomValue val = customFields_.at(fieldName);
-    }
-    catch(std::out_of_range error){
-        throw mm::Unknown_Field();
-    }
-    customFields_[fieldName].reset();
-    customFields_[fieldName] = CustomValue(value);
-}
