e0d86627097405da7ef7fba18c40a90a730727c6
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2015-10-11 18:21:21 -0300

    new CustomObject unit tests

--- tests/ut_libMatchMakerData/ut_customobject.cpp
+++ tests/ut_libMatchMakerData/ut_customobject.cpp
@@ -11,9 +11,9 @@
 TEST (CustomObject, addField) {
     mm::CustomObject customObject;
     std::string s;
-    EXPECT_THROW(customObject.get<std::string>("field", s), mm::Unknown_Field);
+    EXPECT_FALSE(customObject.contains("field"));
     customObject.addField("field");
-    EXPECT_FALSE(customObject.get<std::string>("field", s));
+    EXPECT_TRUE(customObject.contains("field"));
 }
 
 TEST(CustomObject, setValue){
@@ -22,29 +22,26 @@ TEST(CustomObject, setValue){
     customObject.addField("field");
 
     customObject.set<std::string>("field", "teste");
-    std::string fieldValue;
-    bool getWorked = customObject.get<std::string>("field", fieldValue);
 
-    EXPECT_TRUE(getWorked);
+    std::string fieldValue = customObject.value<std::string>("field");
 
     EXPECT_EQ(fieldValue, std::string("teste"));
 }
 
 TEST(CustomObject, getNullValue){
+
     mm::CustomObject customObject;
     customObject.addField("field");
-    std::string fieldValue;
-    bool worked = customObject.get<std::string>("field", fieldValue);
-    EXPECT_FALSE(worked);
+    EXPECT_TRUE(customObject.contains("field"));
+    EXPECT_TRUE(customObject.isNull("field"));
+    EXPECT_THROW(customObject.value<std::string>("field"), mm::Null_Field);
 }
 
 TEST(CustomObject, getValue){
     mm::CustomObject customObject;
     customObject.addField("field");
     customObject.set<std::string>("field", "valor");
-    std::string fieldValue;
-    bool worked = customObject.get<std::string>("field", fieldValue);
-    EXPECT_TRUE(worked);
+    std::string fieldValue = customObject.value<std::string>("field");
     EXPECT_EQ(fieldValue, std::string("valor"));
 }
 
